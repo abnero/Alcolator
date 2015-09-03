e8c6e616e17ecda66a47c13a84613c4a7f8d6a0c
abnero <abnero@gmail.com>
2015-09-03 13:47:05 -0700

    Issue with Simulator

---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Abner Flores on 9/1/15.
+//  Copyright (c) 2015 Abner Flores. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
